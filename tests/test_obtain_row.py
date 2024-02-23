8f15fad61568392079a568d757d01c8216c74eb2
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-02-22 20:18:26 -0800

    ♻️ Improve expresividad

--- tests/test_obtain_row.py
+++ tests/test_obtain_row.py
@@ -7,3 +7,11 @@ def test_obtain_name_team():
     expected_name_team = "Cruz Azul"
     obtained_name_team = gts.obtain_name_team(raw_data)
     assert obtained_name_team == expected_name_team
+    assert_a_real_example_from_json_file()
+
+
+def assert_a_real_example_from_json_file():
+    expected_name_team = "Cruz Azul"
+    real_example = gts.read_json("tests/data/data_file_262_2295.json")
+    obtained_name_team = gts.obtain_name_team(real_example)
+    assert obtained_name_team == expected_name_team
