32754909943697e799262275411b0c0143ec6b4b
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-02-22 20:10:41 -0800

    ✅ Pass test

--- goals_from_team_statistic/__init__.py
+++ goals_from_team_statistic/__init__.py
@@ -2,3 +2,4 @@
 
 __version__ = "0.1.0"
 from .io import *  # noqa
+from .obtain_row import *  # noqa
---
+++ goals_from_team_statistic/obtain_row.py
@@ -0,0 +1,2 @@
+def obtain_name_team(teams_statistics):
+    return teams_statistics["response"]["team"]["name"]
